a766187aab545e54eb3d2e0ce464e08ed5aa8423
lojones <lojones@oak.fg.rbc.com>
2018-03-11 11:35:04 -0400

    genesis

---
+++ src/main/java/com/lojo/apps/scheduler/complexscheduler/ComplexschedulerApplication.java
@@ -0,0 +1,12 @@
+package com.lojo.apps.scheduler.complexscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplexschedulerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lojo/apps/scheduler/complexscheduler/ComplexschedulerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lojo.apps.scheduler.complexscheduler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComplexschedulerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
